fe45a48a0437aa8bfbdeaf0146cbb0eedadda212
dTravis23 <datra5@morgan.edu>
2026-06-01 19:09:04 -0400

    - Formatted http responses on frontend - fixed front end functions

---
+++ Main.py
@@ -0,0 +1,142 @@
+import requests
+
+# TO-DO:
+# 1. Format data from backend, its ugly
+# 2. Add additional features. Due dates? Priority list?
+# 3. Polish terminal interface -> Colors?
+
+def view_tasks():
+    
+    specificTask = input("""
+    View a specific task? 
+    y - Yes
+    n - No
+    
+    Selection: """)
+    
+    if specificTask.lower() == 'n':
+        
+        view_tasks_response = requests.get("http://127.0.0.1:8000/tasks")
+        display_tasks(response=view_tasks_response, display_multiple_tasks=True)
+        return view_tasks_response.json()
+    
+    elif specificTask.lower() == 'y':
+        
+        task_to_view = int(input('\n' +  "Enter the task ID you would like to view: "))
+        view_tasks_response = requests.get(f"http://127.0.0.1:8000/tasks/{task_to_view}")
+        
+        display_tasks(response=view_tasks_response, display_multiple_tasks=False)
+        
+        return view_tasks_response.json()
+    
+    else:
+        print("Invalid response, please try again!")
+
+def create_task():
+    
+    taskName = input('\n' + "Enter a name for the task: ")
+    
+    newTask = {
+        'task_name':taskName,
+        'task_id': 0,
+        'is_done':False
+    }
+    
+    add_task_response = requests.post("http://127.0.0.1:8000/tasks", json=newTask)
+    
+    print('\n' + f"Task '{taskName}' successfully created!")
+    display_tasks(response=add_task_response, display_multiple_tasks=False)
+    
+def remove_task():
+    specificTask = input("""
+    Remove a specific task?
+    
+    y - Yes
+    n - No
+    
+    Selection: """)
+    
+    if specificTask.lower() == "n":
+        
+        delete_task_response = requests.delete("http://127.0.0.1:8000/tasks/last")
+        display_tasks(response=delete_task_response, display_multiple_tasks=True)
+        
+    elif specificTask.lower() == "y":
+        
+        task_to_delete = input("""
+    Enter the task ID you would like to remove:
+    
+    Selection: """)
+        
+        delete_task_response = requests.delete(f"http://127.0.0.1:8000/tasks/{task_to_delete}/")
+        display_tasks(response=delete_task_response, display_multiple_tasks=True)
+        
+    else:
+        print("Invalid selection. Try again!")
+        
+def update_task():
+    task_to_update = input('\n' + "Enter the ID of the task you would like to update: ")
+    
+    if not task_to_update.isnumeric():
+        
+        print("Please enter a valid task ID.")
+        
+    else:
+        
+        update_task_response = requests.patch(f"http://127.0.0.1:8000/tasks/{task_to_update}")
+        print('\n' + f"Task '{update_task_response.json().get('data').get('task_name')}' has been successfully updated!")
+        
+def display_tasks(response: requests.Response, display_multiple_tasks: bool):
+    
+    response_dict = response.json()
+    
+    if display_multiple_tasks == True:
+        
+        print('\n' + "My Tasks: ")
+        
+        for task in response_dict["data"]:
+                
+                if task["is_done"] == True:
+                    status = "✔️"
+                else: 
+                    status = "❌"
+                
+                print(f"[{task['task_id']}] {task['task_name']} {status}")
+    else:
+        
+        response_dict = response_dict["data"]
+        
+        if response_dict.get("data") == True:
+            status = "✔️"
+            print(f"[{response_dict.get('task_id')}] {response_dict.get('task_name')} {status}")
+            
+        else: 
+            status = "❌"
+            print(f"[{response_dict.get('task_id')}] {response_dict.get('task_name')} {status}")
+
+menuOptions = {
+    "1":view_tasks,
+    "2":create_task,
+    "3":remove_task,
+    "4":update_task,
+    "5":exit
+}
+
+def menuSelect():
+    
+    menuResponse = input("""
+    __________________________
+    What would you like to do?
+
+    1 - View Tasks
+    2 - Add a Task
+    3 - Remove a Task
+    4 - Update a Task
+    5 - Exit
+
+    Selection: """)
+
+    # Run associated functions
+    if menuResponse in menuOptions: menuOptions[menuResponse]()
+        
+while True: menuSelect()
---
+++ Routes.py
@@ -0,0 +1,155 @@
+from typing import Annotated, TypeVar, Generic
+from datetime import datetime
+from fastapi import Depends, FastAPI, HTTPException
+from fastapi.concurrency import asynccontextmanager
+from pydantic import BaseModel
+from random import randint
+from sqlalchemy import create_engine
+from sqlmodel import Field, SQLModel, Session, delete, select
+
+# Database file and url to create
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+# Engine instance
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, connect_args=connect_args)
+
+# Create the database
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+    
+# Create session for engine to interact with database
+def get_session():
+    with Session(engine) as session:
+        yield session
+SessionDep = Annotated[Session, Depends(get_session)]
+
+# Task object for SQLite
+class Task(SQLModel, table=True):
+    task_id : int | None = Field(default=None, primary_key=True)
+    task_name : str = Field(index=True,)
+    # due_date : datetime | None = None
+    is_done : bool = False
+    
+# Generic response model
+T = TypeVar('T')
+class Response(BaseModel, Generic[T]):
+    data: T
+    
+# Response model for creating/udpating
+class TaskCreate(SQLModel):
+    task_name : str = Field(index=True)
+    is_done : bool = Field(default=False)
+    
+# Create database on startup with test tables
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_db_and_tables()
+    # with Session(engine) as session:
+    #     if not session.exec(select(Task)).first():
+    #         # session.add_all([
+    #         #     Task
+    #         # ])
+    #         # session.commit()
+    yield
+
+# Initialize the FastAPI app
+taskManager = FastAPI(root_path="/api/v1", lifespan=lifespan)
+
+# Define a route for the root URL ("/") that returns a simple JSON response
+@taskManager.get("/")
+async def home():
+    return {"Status: ": "Task Manager API is running!"}
+
+# Read all tasks from database
+@taskManager.get("/tasks", response_model=Response[list[Task]])
+async def read_tasks(session: SessionDep):
+    all_tasks = session.exec(select(Task)).all()
+    return {"data" : all_tasks}
+
+# Read task with ID
+@taskManager.get("/tasks/{task_id}", response_model=Response[Task])
+async def read_task_by_id(task_id: int, session: SessionDep):
+    
+    this_task = session.get(Task, task_id)
+    if not this_task:
+        raise HTTPException(404, "Not found - Task does not exist")
+    
+    return {'data' : this_task}
+
+# Create a new task
+@taskManager.post("/tasks", response_model=Response[Task])
+async def create_task(task: TaskCreate, session: SessionDep):
+    
+    # Pass in TaskCreate object which omits automatic fields
+    db_task = Task.model_validate(task)
+    session.add(db_task)
+    session.commit()
+    session.refresh(db_task)
+        
+    return {'data' : db_task}
+
+# Change is_done value of a task
+@taskManager.patch("/tasks/{task_id}", response_model=Response[Task])
+async def update_task_status(task_id: int, session: SessionDep):
+    
+    updated_task = session.get(Task, task_id)
+    print(updated_task)
+    
+    if not updated_task:
+        raise HTTPException(404, "Not found - Task does not exist")
+    
+    updated_task.is_done = not updated_task.is_done
+    session.commit()
+    session.refresh(updated_task)
+    
+    return {'data' : updated_task}
+
+# Delete the last task
+@taskManager.delete("/tasks/last", response_model=Response[list[Task]])
+async def remove_last_task(session: SessionDep):
+    
+    last_task = session.exec(
+        select(Task)
+        .order_by(Task.task_id.desc())
+        ).first()
+    
+    if not last_task:
+        raise HTTPException(404, "Not found! -- no tasks to delete")
+    
+    session.delete(last_task)
+    session.commit()
+        
+    remaining_tasks = session.exec(
+        select(Task)
+        .order_by(Task.task_id)
+    ).all()
+        
+    return {'data' : remaining_tasks}
+    
+# Delete specific task
+@taskManager.delete("/tasks/{task_id}", response_model=Response[list[Task]])
+async def remove_specific_task(task_id: int, session: SessionDep):
+    
+    task_to_delete = session.get(Task, task_id)
+    if not task_to_delete:
+        raise HTTPException(404, "Not found! -- task does not exist")
+    
+    session.delete(task_to_delete)
+    session.commit()
+        
+    remaining_tasks = session.exec(select(Task)).all()
+    
+    return {'data' : remaining_tasks}
+
+# Clear the task list
+@taskManager.delete("/tasks")
+async def clear_tasks(session: SessionDep):
+    
+    session.exec(delete(Task))
+    session.commit()
+    
+    raise HTTPException(204, "Tasks have been cleared.")
+    
+print("Successfully compiled!")
